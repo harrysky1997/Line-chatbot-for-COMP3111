8f3ac0d6bfa3161bc6d7e0ea1ac066ce06ffc65f
harrysky1997 <kayusze1997@gmail.com>
2017-11-01 19:02:13 +0800

    Test #7

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -279,7 +279,7 @@ public class KitchenSinkController {
             	String imageUrl = createUri("/static/buttons/1040.jpg");
             	Action[] tourEnroll = new Action[4];
             	
-            	for (int i = 0; i < 4; i++) {
+            	for (int i = 0; i < tour.size(); i++) {
             		tourEnroll[i] = 
             		new MessageAction(tour.get(i), "You successfully enroll in " + tour.get(i) + ".");
             	}
